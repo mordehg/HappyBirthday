69cae7a7afecfbb6a04ab8976f4c270fc17a7ae2
Gili Yaakov <giliyaakov@h-MacBook-Pro-sl-Gili.local>
2021-08-01 09:39:43 +0300

    new project

---
+++ HappyBirthday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyBirthday
+//
+//  Created by Gili Yaakov on 31/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
